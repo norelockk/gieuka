6676bb754a5b24b5b3cc7d9b502751f8dc0f22f3
norelockk <norbert.podbieglyyy@gmail.com>
2024-12-11 13:28:28 +0100

    feat: add TerserPlugin for JavaScript minification in production build

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,7 +1,35 @@
 const { merge } = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
 const common = require('./webpack.common.js');
+
+const reactDomProps = require('react-dom-props')
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: 'source-map'
+    devtool: 'source-map',
+    optimization: {
+        minimize: true, // Enable minimization
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    module: false,
+                    mangle: {
+                        properties:  {
+                            reserved: require('../shared/props.json'),
+                            keep_quoted: true,
+                        },
+                    },
+                    compress: {
+                        join_vars: false,
+                        drop_console: true,
+                        unsafe_methods: true,
+                    },
+                    format: {
+                        comments: false,
+                    },
+                },
+                extractComments: true,
+            }),
+        ],
+    },
 });
